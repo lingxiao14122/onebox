648a2a0188aa67b3afff7ee1fc44d7641e20b43c
ling xiao <lingxiao3218@gmail.com>
2022-09-20 01:47:01 +0800

    feat: select product form enhance

--- resources/js/selectProduct.js
+++ resources/js/selectProduct.js
@@ -1,5 +1,3 @@
-import { drop } from "lodash";
-
 /**
  * 
  * @param {HTMLInputElement} elem 
@@ -12,17 +10,17 @@ async function chooseProduct(elem) {
     }
 
     removeListeners(elem)
-    elem.addEventListener('blur', whenElementBlur);
-    elem.addEventListener('input', whenKeydown)
+    elem.addEventListener('blur', whenBlur);
+    elem.addEventListener('input', whenInput)
 
 
     let json = await requestAllItems()
     let dropdownbox = ""
     if (json[0] === "axiosError") {
-        dropdownbox = `<div id="dropdownbox" class="absolute w-full top-[60px] max-h-60 overflow-auto">Could not fetch items from server😢</div>`
+        dropdownbox = `<div id="dropdownbox" class="dropdownbox z-10 absolute w-full top-[60px] max-h-60 overflow-auto bg-white">Could not fetch items from server😢</div>`
     } else {
         let slot = jsonToDropdownList(json)
-        dropdownbox = `<div id="dropdownbox" class="absolute w-full top-[60px] max-h-60 overflow-auto">${slot}</div>`
+        dropdownbox = `<div id="dropdownbox" class="dropdownbox z-10 absolute w-full top-[60px] max-h-60 overflow-auto bg-white">${slot}</div>`
     }
 
     elem.parentElement.insertAdjacentHTML('beforeend', dropdownbox)
@@ -43,16 +41,16 @@ async function requestAllItems() {
     }
 }
 
-async function whenKeydown(e) {
+async function whenInput(e) {
     // throttle (less endpoint hit)
     if (e.target.value.length > 0 && e.target.value.length < 3) {
         populateDataDropdownBox([])
         return
     }
-    let input = e.target.value
+    let inputValue = e.target.value
     let response
     try {
-        response = await window.axios.get(`/api/item/search?q=${input}`)
+        response = await window.axios.get(`/api/item/search?q=${inputValue}`)
     } catch (error) {
         console.log(error)
         // terminate this function
@@ -70,19 +68,22 @@ function populateDataDropdownBox(json) {
 }
 
 
-function whenElementBlur(e) {
-    let input = e.target
-    input.dataset.clicked = false
-    removeListeners(input)
+function whenBlur(e) {
+    let inputElement = e.target
+    inputElement.dataset.clicked = false
+    removeListeners(inputElement)
+    const dropdownbox = inputElement.parentElement.getElementsByClassName('dropdownbox')[0];
     // delay so that dropdown item can be clicked before removed from dom
+    dropdownbox.classList.add("opacity-0")
     setTimeout(() => {
-        document.getElementById('dropdownbox').remove()
-    }, 200);
+        // document.getElementById('dropdownbox') ?? document.getElementById('dropdownbox').remove()
+        dropdownbox.remove()
+    }, 500)
 }
 
 function removeListeners(elem) {
-    elem.removeEventListener('blur', whenElementBlur)
-    elem.removeEventListener('keydown', whenKeydown)
+    elem.removeEventListener('blur', whenBlur)
+    elem.removeEventListener('keydown', whenInput)
 }
 
 
@@ -150,12 +151,16 @@ let dropdown = `<div id="dropdownbox" class="absolute w-full top-[60px] max-h-60
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 function chooseProductSave(elem, productId, productName) {
-    // validate
-    let dropdownbox = elem.parentElement.parentElement.parentElement
-    for (let i = 1; i < dropdownbox.children.length; i++) {
-        let listitem = dropdownbox.children[i].children[1].value
-        console.log(i)
-        if (productId == listitem) {
+    // validate self
+    console.log(elem.parentElement.parentElement)
+    // validate current list same product
+    let selectProductDiv = elem.parentElement.parentElement.parentElement
+    for (let i = 1; i < selectProductDiv.children.length - 1; i++) {
+        let row = selectProductDiv.children[i]
+        let parentInputElement = row.children[1] // second input (hidden one)
+        console.log(parentInputElement)
+        if (productId == parentInputElement.value) {
+            elem.parentElement.remove()
             alert('Cannot select same product')
             return
         }
@@ -164,15 +169,48 @@ function chooseProductSave(elem, productId, productName) {
     // insert to dom
     elem.parentElement.parentElement.children[0].value = productName
     elem.parentElement.parentElement.children[1].value = productId
-    dropdownbox.insertAdjacentHTML('beforeend', 
+    selectProductDiv.insertAdjacentHTML('beforeend', 
     `
-    <div class="relative flex items-center h-14">
-        <input class="h-12 pl-6 mr-4 basis-1/2" type="text" value="" placeholder="Select a product" onclick="chooseProduct(this)">
-        <input type="hidden" name="items[]" type="number">
-        <input class="basis-1/2 max-w-[200px] h-8" type="number" value="" name="itemsCount[] required">
+    <div data-role="row" class="relative flex items-center h-14">
+        <input class="h-12 pl-6 mr-4 basis-1/2" type="text" value="" placeholder="Select a product"
+            onclick="chooseProduct(this)">
+        <input type="hidden" name="item_ids[]" type="number">
+        <input class="basis-1/2 max-w-[200px] h-8" type="number" min="1" name="item_counts[]"
+            required />
+        <div class="mx-4 cursor-pointer" onclick="chooseProductRowDelete(this)">
+            <?xml version="1.0" encoding="UTF-8"?><svg width="24" height="24" viewBox="0 0 48 48"
+                fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8 11L40 11" stroke="#333" stroke-width="4" stroke-linecap="round"
+                    stroke-linejoin="round" />
+                <path d="M18 5L30 5" stroke="#333" stroke-width="4" stroke-linecap="round"
+                    stroke-linejoin="round" />
+                <path d="M12 17H36V40C36 41.6569 34.6569 43 33 43H15C13.3431 43 12 41.6569 12 40V17Z"
+                    fill="none" stroke="#333" stroke-width="4" stroke-linejoin="round" />
+                <path d="M20 25L28 33" stroke="#333" stroke-width="4" stroke-linecap="round"
+                    stroke-linejoin="round" />
+                <path d="M28 25L20 33" stroke="#333" stroke-width="4" stroke-linecap="round"
+                    stroke-linejoin="round" />
+            </svg>
+        </div>
     </div>
     `
     )
+    elem.parentElement.remove()
 }
 
 window.chooseProductSave = chooseProductSave
+
+/**
+ * 
+ * @param {HTMLDivElement} elem 
+ */
+function chooseProductRowDelete(elem) {
+    let selectProductDiv = elem.parentElement.parentElement
+    if (selectProductDiv.childElementCount > 2) {
+        elem.parentElement.remove()
+    } else {
+        alert("Cannot remove last row")
+    }
+}
+
+window.chooseProductRowDelete = chooseProductRowDelete
--- resources/views/transaction/create.blade.php
+++ resources/views/transaction/create.blade.php
@@ -4,7 +4,7 @@
         <h1 class="text-3xl font-semibold">Record Transaction</h1>
         <a href="{{ url('/transaction') }}" class="underline hover:text-blue-500">Go back</a>
     </div>
-    <form action="{{ url('/transaction/create') }}" method="get" class="mx-5">
+    <form action="{{ url('/transaction') }}" method="post" class="mx-5">
         @csrf
         <div class="w-96">
             <div class="mb-4">
@@ -22,17 +22,68 @@
             <label for="products" class="block mb-2 text-lg">Select product
                 <span class="text-red-600">*</span>
             </label>
-            <div class="w-full mb-[240px] border rounded border-slate-400">
+            @error('item_ids')
+                <p class="mt-1 mb-2 text-red-500">{{ $message }}</p>
+            @enderror
+            @error('item_counts')
+                <p class="mt-1 mb-2 text-red-500">{{ $message }}</p>
+            @enderror
+            <div id="selectProductDiv" class="w-full mb-6 border rounded border-slate-400">
                 <div class="flex py-4 pl-6 font-medium">
                     <h5 class="basis-1/2">Product</h5>
                     <h5 class="basis-1/2">Count</h5>
                 </div>
-                <div class="relative flex items-center h-14">
-                    <input class="h-12 pl-6 mr-4 basis-1/2" type="text" value="" placeholder="Select a product" onclick="chooseProduct(this)">
-                    <input type="hidden" name="items[]" type="number">
-                    <input class="basis-1/2 max-w-[200px] h-8" type="number" value="" name="itemsCount[] required">
+                <div data-role="row" class="relative flex items-center h-14">
+                    <input class="h-12 pl-6 mr-4 basis-1/2" type="text" value="" placeholder="Select a product"
+                        onclick="chooseProduct(this)">
+                    <input type="hidden" name="item_ids[]" type="number">
+                    <input class="basis-1/2 max-w-[200px] h-8" type="number" min="1" name="item_counts[]"
+                        required />
+                    <div class="mx-4 cursor-pointer" onclick="chooseProductRowDelete(this)">
+                        <?xml version="1.0" encoding="UTF-8"?><svg width="24" height="24" viewBox="0 0 48 48"
+                            fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8 11L40 11" stroke="#333" stroke-width="4" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                            <path d="M18 5L30 5" stroke="#333" stroke-width="4" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                            <path d="M12 17H36V40C36 41.6569 34.6569 43 33 43H15C13.3431 43 12 41.6569 12 40V17Z"
+                                fill="none" stroke="#333" stroke-width="4" stroke-linejoin="round" />
+                            <path d="M20 25L28 33" stroke="#333" stroke-width="4" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                            <path d="M28 25L20 33" stroke="#333" stroke-width="4" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                        </svg>
+                    </div>
                 </div>
             </div>
+        </div>
+        <script>
+            function clickSubmitBtn(event) {
+                let selectProductDiv = document.getElementById('selectProductDiv');
+                let lastRow = selectProductDiv.lastElementChild
+                // if one field exist dont remove last element
+                if (selectProductDiv.childElementCount <= 2 && (lastRow.children[1].value.length == 0 && lastRow.children[2].value.length == 0)) {
+                    alert('Please select at least one product')
+                    return false
+                } 
+                // if last row not used
+                if (lastRow.children[1].value.length == 0 && lastRow.children[2].value.length == 0) {
+                    lastRow.remove()
+                    document.getElementById('real-submit-btn').click() // help block ui when required validation not pass
+                }
+            }
+            window.clickSubmitBtn = clickSubmitBtn
+        </script>
+        <div class="mb-6">
+            {{-- dispatch submit event --}}
+            <button class="px-4 py-2 text-white bg-blue-500 rounded hover:bg-black"
+                onclick="clickSubmitBtn(event);">
+                Save
+            </button>
+            {{-- receive submit event --}}
+            <button id="real-submit-btn" type="submit" class="hidden">
+                Save
+            </button>
         </div>
     </form>
 </x-layout>
