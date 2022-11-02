fbb6a393adc8ea6f562fd93a98a3c131b37a0a33
xiao <lingxiao3218@gmail.com>
2022-11-02 08:10:49 +0000

    fix: remove unused markup

--- resources/views/integration/index.blade.php
+++ resources/views/integration/index.blade.php
@@ -28,17 +28,5 @@
                 </a>
             </div>
         @endif
-        <div class="relative border border-gray-500 rounded w-60">
-            <div class="absolute w-full">
-                <img class="object-scale-down h-32 m-auto w-52"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Shopify_logo_2018.svg/1024px-Shopify_logo_2018.svg.png">
-                <hr class="border-slate-500">
-                <div class="flex items-baseline gap-2 p-4">
-                    <p class="text-2xl font-semibold">Shopify</p>
-                    {{-- <a class="text-lg text-blue-500 hover:underline" href="{{ url('/integration/auth/lazada') }}">Authorize</a> --}}
-                </div>
-            </div>
-            <div class="absolute text-3xl rotate-12 translate-x-5 translate-y-[5rem]">Coming soon</div>
-        </div>
     </div>
 </x-layout>
